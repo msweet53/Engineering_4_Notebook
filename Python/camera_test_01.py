974cb8c736e12204603f59af1b373dd1300c8087
msweet53 <maxsweet511@gmail.com>
2021-12-09 14:29:43 +0000

    finished script for taking pictures

--- Python/camera_test_01.py
+++ Python/camera_test_01.py
@@ -4,6 +4,8 @@ import picamera
 with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
     camera.start_preview()
+    print("running")
     # Camera warm-up time
     time.sleep(2)
     camera.capture('currentimage.jpg')
+    print("finished")

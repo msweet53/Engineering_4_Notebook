cb6cce3107e2d500ab3a3a618bfdf38dd7f0820a
msweet53 <maxsweet511@gmail.com>
2021-12-13 15:10:55 +0000

    added 5 mode camera

--- Python/camera_test_02.py
+++ Python/camera_test_02.py
@@ -3,8 +3,37 @@ import picamera
 
 with picamera.PiCamera() as camera:
     camera.image_effect = 'negative'
+    print("capturing negative")
     camera.resolution = (1024, 768)
     camera.start_preview()
     # Camera warm-up time
     time.sleep(2)
-    camera.capture('urmom2.png')
+    camera.capture('negative.jpg')
+    camera.image_effect = 'solarize'
+    print("capturing solarize")
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    # Camera warm-up time
+    time.sleep(2)
+    camera.capture('solarize.jpg')
+    camera.image_effect = 'sketch'
+    print("capturing sketch")
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    # Camera warm-up time
+    time.sleep(2)
+    camera.capture('sketch.jpg')
+    camera.image_effect = 'denoise'
+    print("capturing denoise")
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    # Camera warm-up time
+    time.sleep(2)
+    camera.capture('denoise.jpg')
+    camera.image_effect = 'emboss'
+    print("capturing emboss")
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    # Camera warm-up time
+    time.sleep(2)
+    camera.capture('emboss.jpg')
